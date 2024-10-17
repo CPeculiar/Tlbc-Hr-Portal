65f455946aa0a8c2664a0f13146e1a435e8534ef
CPeculiar <pecudamicable@gmail.com>
2024-10-17 11:33:51 +0100

    Vercel fixes 4

--- src/utils/ThemeContext.js
+++ src/utils/ThemeContext.js
@@ -1,22 +1,66 @@
-import * as React from 'react';
+// import * as React from 'react';
+
+// const ThemeContext = React.createContext({
+//   currentTheme: 'light',
+//   changeCurrentTheme: () => {},
+// });
+
+// export default function ThemeProvider({children}) {  
+//   const persistedTheme = localStorage.getItem('theme');
+//   const [theme, setTheme] = React.useState(persistedTheme || 'light');
+
+//   const changeCurrentTheme = (newTheme) => {
+//     setTheme(newTheme);
+//     localStorage.setItem('theme', newTheme);
+//   };
+
+//   React.useEffect(() => {
+//     document.documentElement.classList.add('[&_*]:!transition-none');
+//     if (theme === 'light') {
+//       document.documentElement.classList.remove('dark');
+//       document.documentElement.style.colorScheme = 'light';
+//     } else {
+//       document.documentElement.classList.add('dark');
+//       document.documentElement.style.colorScheme = 'dark';
+//     }
+
+//     const transitionTimeout = setTimeout(() => {
+//       document.documentElement.classList.remove('[&_*]:!transition-none');
+//     }, 1);
+    
+//     return () => clearTimeout(transitionTimeout);
+//   }, [theme]);
+
+//   return (
+//     <>
+//     <ThemeContext.Provider value={{ currentTheme: theme, changeCurrentTheme }}>
+//       {children}
+//     </ThemeContext.Provider>
+//     </>
+//   );
+// }
+
+// export const useThemeProvider = () => React.useContext(ThemeContext);
+
+
 
 import React, { createContext, useState, useEffect, useContext } from 'react';
 
-const ThemeContext = React.createContext({
+const ThemeContext = createContext({
   currentTheme: 'light',
   changeCurrentTheme: () => {},
 });
 
-export default function ThemeProvider({children}) {  
+export function ThemeProvider({ children }) {
   const persistedTheme = localStorage.getItem('theme');
-  const [theme, setTheme] = React.useState(persistedTheme || 'light');
+  const [theme, setTheme] = useState(persistedTheme || 'light');
 
   const changeCurrentTheme = (newTheme) => {
     setTheme(newTheme);
     localStorage.setItem('theme', newTheme);
   };
 
-  React.useEffect(() => {
+  useEffect(() => {
     document.documentElement.classList.add('[&_*]:!transition-none');
     if (theme === 'light') {
       document.documentElement.classList.remove('dark');
@@ -29,17 +73,15 @@ export default function ThemeProvider({children}) {
     const transitionTimeout = setTimeout(() => {
       document.documentElement.classList.remove('[&_*]:!transition-none');
     }, 1);
-    
+
     return () => clearTimeout(transitionTimeout);
   }, [theme]);
 
-  return (
-    <>
-    <ThemeContext.Provider value={{ currentTheme: theme, changeCurrentTheme }}>
-      {children}
-    </ThemeContext.Provider>
-    </>
+  return React.createElement(
+    ThemeContext.Provider,
+    { value: { currentTheme: theme, changeCurrentTheme } },
+    children
   );
 }
 
-export const useThemeProvider = () => React.useContext(ThemeContext);
+export const useThemeProvider = () => useContext(ThemeContext);
