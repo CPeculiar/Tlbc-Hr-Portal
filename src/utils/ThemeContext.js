2e71316e027f137746a97d75c556ec28ed0598d7
CPeculiar <pecudamicable@gmail.com>
2024-10-17 12:29:53 +0100

    Vercel fixes again

--- src/utils/ThemeContext.js
+++ src/utils/ThemeContext.js
@@ -86,3 +86,4 @@ export function ThemeProvider({ children }) {
 }
 
 export const useThemeProvider = () => useContext(ThemeContext);
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 // import plugin from "tailwindcss/plugin";
 // import forms from '@tailwindcss/forms';
-import plugin from 'tailwindcss/plugin';
+const plugin = require('tailwindcss/plugin');
 const forms = require('@tailwindcss/forms');
 // const plugin = require('@tailwindcss/plugin')
 
